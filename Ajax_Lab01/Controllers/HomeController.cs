54fa538613dfcfccb6b5bb3ee78393774a61b40a
jackeya307 <a30786531@gmail.com>
2024-08-12 00:50:01 +0800

    會員註冊作業

--- Ajax_Lab01/Controllers/HomeController.cs
+++ Ajax_Lab01/Controllers/HomeController.cs
@@ -1,18 +1,17 @@
 using Ajax_Lab01.Models;
-
+using Ajax_Lab01.ViewModels;
 using Microsoft.AspNetCore.Mvc;
-
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Diagnostics;
-using System.Text;
 
 namespace Ajax_Lab01.Controllers {
     public class HomeController : Controller {
-        private readonly ILogger<HomeController> _logger;
+        private readonly IWebHostEnvironment _webHost;
         private MyDBContext _dbContext;
-        public HomeController(ILogger<HomeController> logger,MyDBContext dbContext)
+        public HomeController(MyDBContext dbContext,IWebHostEnvironment webHost)
         {
-            _logger = logger;
             _dbContext = dbContext;
+            _webHost = webHost;
         }
 
         [HttpGet("/Home/SiteId/{City?}")] //
@@ -25,7 +24,9 @@ namespace Ajax_Lab01.Controllers {
             .ToList();
 
             if(!c.Any()) {
-                return NotFound();
+                return NotFound(new {
+                    error = "搜尋不到此城市"
+                });
             }
             return Json(c);
         }
@@ -40,9 +41,11 @@ namespace Ajax_Lab01.Controllers {
                 .Select(Address => Address.Road)
                 .Distinct()
                 .ToList();
+
             if(!c.Any()) {
                 return NoContent();
             }
+
             return Json(c);
         }
         public IActionResult Index()
@@ -59,6 +62,75 @@ namespace Ajax_Lab01.Controllers {
         {
             return View();
         }
+        public IActionResult UserPhoto()
+        {
+
+            ViewBag.MemberID = new SelectList(_dbContext.Members.Select(c => c.MemberId));
+            return View();
+        }
+
+        public async Task<IActionResult> getImg(int? id)
+        {
+            try {
+                if(id == null)
+                    return NotFound(new {
+                        error = "id不能為空"
+                    });
+                //var c = _dbContext.Members.Find(id).FileData; 如果Find找不到就會回傳null 但繼續找filedata會跳異常
+
+                Member? c = await _dbContext.Members.FindAsync(id);
+
+                if(c != null && c.FileData != null) {
+                    return File(c.FileData,"image/jpeg");
+                }
+
+                return NotFound(new {
+                    error = "無此ID"
+                });
+            }
+            catch(Exception ex) {
+                return NotFound("發生例外的狀況 " + ex.Message);
+            }
+        }
+        public IActionResult Register(UserInfo user)
+        {
+            if(user == null || user.UserPhoto == null || user.UserPhoto.Length == 0)
+                return BadRequest(new {
+                    error = "圖片或資料未填入"
+                });
+            try {
+                Byte[]? data;
+                using(var memory = new MemoryStream()) {
+                    user.UserPhoto[0].CopyTo(memory);
+                    data = memory.ToArray();
+                }
+
+                string path = $"{_webHost.WebRootPath}/Images/{user.UserPhoto[0].FileName}";
+                using(var filestream = new FileStream(path,FileMode.Create)) {
+                    user.UserPhoto[0].CopyTo(filestream);
+                }
+
+                Member member = new Member() {
+                    Email = user.Email,
+                    Name = user.Name,
+                    Age = user.Age,
+                    Password = user.Password,
+                    FileData = data,
+                    FileName = user.UserPhoto[0].FileName,
+                };
+                _dbContext.Members.Add(member);
+                _dbContext.SaveChanges();
+
+                return Json("Done");
+            }
+            catch(Exception ex) {
+                return NotFound(new {
+                    error = ex.Message + "發生例外的狀況",
+                });
+
+            }
+        }
+
         [ResponseCache(Duration = 0,Location = ResponseCacheLocation.None,NoStore = true)]
         public IActionResult Error()
         {
---
+++ Ajax_Lab01/ViewModels/UserInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace Ajax_Lab01.ViewModels {
+    public class UserInfo {
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public int? Age { get; set; }
+
+        public IFormFile[]? UserPhoto { get; set; }
+    }
+}
